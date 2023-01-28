1f762e9a6db98476ec8694eb7219aa42680708ae
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-01-28 11:34:43 +0000

    .

--- rails-api-auth/config/routes.rb
+++ rails-api-auth/config/routes.rb
@@ -5,6 +5,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   scope :api, defaults: { format: :json } do
-    devise_for :users
+    devise_for :users, controllers: { sessions: :sessions }, path_names: { sign_in: :login }
   end
 end
