25191184e5f2436a2069e5945e5d00b504bf5cb7
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-01-28 11:55:28 +0000

    .

--- rails-api-auth/config/routes.rb
+++ rails-api-auth/config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  # root "layouts#application"
   scope :api, defaults: { format: :json } do
     devise_for :users, controllers: { sessions: :sessions }, path_names: { sign_in: :login }
     resource :user, only: [:show, :update]
