d0631f797f82905234581ff1422f2327df9efc1a
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-01-29 16:25:17 +0000

    Create JwtToken Concern

--- demo-jwt/app/controllers/application_controller.rb
+++ demo-jwt/app/controllers/application_controller.rb
@@ -1,2 +1,19 @@
 class ApplicationController < ActionController::API
+    include JwToken
+
+    before_action :authenticate_user
+
+    private
+        def authenticate_user
+            header = request.header['Authorization']
+            header = header.split(' ').last if header
+            begin
+                @decoded = JwToken.decode(header)
+                @current_user = User.find(@decoded[:user_id])
+            rescue ActiveRecord::RecordNotFound => e
+                render json: { errors: e.message }, status: :unauthorized
+            rescue JWT::DecodeError => e
+                render json: { errors: e.message }, status: :unauthorized
+            end
+        end
 end
